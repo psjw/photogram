2ba43574d82f3155c79dd3574a3cf237ff78eec5
psjw <dlrudtn108@gmail.com>
2021-12-19 15:55:33 +0900

    design and springboot config

---
+++ src/main/java/com/psjw/photogram/PhotogramApplication.java
@@ -0,0 +1,13 @@
+package com.psjw.photogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotogramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psjw/photogram/PhotogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psjw.photogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
